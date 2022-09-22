07b31a8595f611c97cf905878c4d1748c944beb5
fikayo1 <soetandan@gmail.com>
2022-09-22 21:13:10 +0100

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Import funcitons from the calculator module and perform basic funcitons"""
+    """Import  from the calculator module to basic funcitons"""
     from calculator_1 import add, sub, mul, div
     import sys
     args = sys.argv[1:]
+    a = args[0]
+    b = args[1]
+    c = args[2]
     if len(args) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     if args[1] == "+":
-        print(f"{args[0]} {args[1]} {args[2]} = {add(int(args[0]), int(args[2]))}")
+        print(f"{a} {b} {c} = {add(int(a), int(c))}")
     elif args[1] == "-":
-        print(f"{args[0]} {args[1]} {args[2]} = {sub(int(args[0]), int(args[2]))}")
+        print(f"{a} {b} {c} = {sub(int(a), int(c))}")
     elif args[1] == "*":
-        print(f"{args[0]} {args[1]} {args[2]} = {mul(int(args[0]), int(args[2]))}")
+        print(f"{a} {b} {c} = {mul(int(a), int(c))}")
     elif args[1] == "/":
-        print(f"{args[0]} {args[1]} {args[2]} = {div(int(args[0]), int(args[2]))}")
+        print(f"{a} {b} {c} = {div(int(a), int(c))}")
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
