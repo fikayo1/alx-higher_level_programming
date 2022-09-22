7dde45561b25cc988e584e0ca99b081b47a70ff4
fikayo1 <soetandan@gmail.com>
2022-09-22 21:09:02 +0100

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
-args = sys.argv[1:]
-if len(args) != 3:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-if args[1] == "+":
-    print(f"{args[0]} {args[1]} {args[2]} = {add(int(args[0]), int(args[2]))}")
-elif args[1] == "-":
-    print(f"{args[0]} {args[1]} {args[2]} = {sub(int(args[0]), int(args[2]))}")
-elif args[1] == "*":
-    print(f"{args[0]} {args[1]} {args[2]} = {mul(int(args[0]), int(args[2]))}")
-elif args[1] == "/":
-    print(f"{args[0]} {args[1]} {args[2]} = {div(int(args[0]), int(args[2]))}")
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
+if __name__ == "__main__":
+    """Import funcitons from the calculator module and perform basic funcitons"""
+    from calculator_1 import add, sub, mul, div
+    import sys
+    args = sys.argv[1:]
+    if len(args) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if args[1] == "+":
+        print(f"{args[0]} {args[1]} {args[2]} = {add(int(args[0]), int(args[2]))}")
+    elif args[1] == "-":
+        print(f"{args[0]} {args[1]} {args[2]} = {sub(int(args[0]), int(args[2]))}")
+    elif args[1] == "*":
+        print(f"{args[0]} {args[1]} {args[2]} = {mul(int(args[0]), int(args[2]))}")
+    elif args[1] == "/":
+        print(f"{args[0]} {args[1]} {args[2]} = {div(int(args[0]), int(args[2]))}")
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
