80c9a9e129ae454d0624f47179e8b74eef001567
fikayo1 <soetandan@gmail.com>
2022-09-22 20:43:41 +0100

    loading a variable

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """Print the value if a from the variable_load_5 module"""
     from variable_load_5 import a
-    print("{}".format(a))
+
+    print(a)
