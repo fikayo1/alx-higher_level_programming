fd3e13cea36a19c16c4ea87daeb0ae71c0cb0065
fikayo1 <soetandan@gmail.com>
2022-09-22 19:33:57 +0100

    importing calculator

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+print("{:d} + {:d} = {:d}".format(add(a, b)))
+print("{:d} + {:d} = {:d}".format(sub(a, b)))
+print("{:d} + {:d} = {:d}".format(mul(a, b)))
+print("{:d} + {:d} = {:d}".format(div(a, b)))
