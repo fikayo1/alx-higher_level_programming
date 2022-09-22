e18a892b813702156ab711c57327a14b8c329866
fikayo1 <soetandan@gmail.com>
2022-09-22 19:35:11 +0100

    importing calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-print("{:d} + {:d} = {:d}".format(add(a, b)))
-print("{:d} + {:d} = {:d}".format(sub(a, b)))
-print("{:d} + {:d} = {:d}".format(mul(a, b)))
-print("{:d} + {:d} = {:d}".format(div(a, b)))
+if __name__ = '__main__':
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(add(a, b)))
+    print("{:d} + {:d} = {:d}".format(sub(a, b)))
+    print("{:d} + {:d} = {:d}".format(mul(a, b)))
+    print("{:d} + {:d} = {:d}".format(div(a, b)))
