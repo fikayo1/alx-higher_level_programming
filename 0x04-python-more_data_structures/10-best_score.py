1634e9d953f93996b51a795b86b9283bb78bdd8f
fikayo1 <soetandan@gmail.com>
2022-09-29 02:17:49 +0100

    nest score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == {}:
+    if a_dictionary == {} or a_dictionary == None:
         return None
-    a = max(list(a_dictionary.values()))
+    a = max(a_dictionary.values())
     for i in a_dictionary:
         if a_dictionary[i] == a:
             return a_dictionary[i]
