f67206c1db50f680ee023ba24a82b50411a673b7
fikayo1 <soetandan@gmail.com>
2022-10-02 16:38:39 +0100

    roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    string = roman_string.upper()
+    num = 0
+    numbers = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    for i in string:
+        if i not in numbers or roman_string == None or type(roman_string) != str:
+            return 0
+    # if the number is less than the next subtract it from the num
+    for i in range(len(string)):
+        if i != len(string) - 1:
+            if numbers[string[i]] < numbers[string[i+1]]:
+                num -= numbers[string[i]]
+            else:
+                num += numbers[string[i]]
+        else:
+            num += numbers[string[i]]
+    return num
