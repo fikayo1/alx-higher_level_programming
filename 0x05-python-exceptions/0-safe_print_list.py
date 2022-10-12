aea72715089b101b9abbfde1e890919a5266cca2
fikayo1 <soetandan@gmail.com>
2022-10-12 09:14:41 +0100

    safe list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        num = 0
         for i in my_list[:x]:
+            num += 1
             print(i, end="")
+            return num
     except:
         print("An error occured")
