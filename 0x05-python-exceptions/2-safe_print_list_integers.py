a34f3f9a53ce2b95be12ea8a7ebab7816a1678ba
fikayo1 <soetandan@gmail.com>
2022-10-12 10:47:37 +0100

    removed indexerror

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if type(my_list[i]) == int:
                 print("{:d}".format(my_list[i]), end="")
                 n += 1
-        except Exception as exc:
-            print(exc)
+        except(ValueError, TypeError):
+            continue
     print()
     return n
