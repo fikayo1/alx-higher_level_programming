f91651031d816fc0ccb0cdbb894c367bb91d9be7
fikayo1 <soetandan@gmail.com>
2022-10-12 09:32:46 +0100

    printing space

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        num = 0
-        for i in my_list[:x]:
+    num = 0
+    for i in range(x):
+        try:
+            print(my_list[m], end='')
             num += 1
-            if i < x:
-                print(i, end="")
-            else:
-                print(i)
-        return num
-    except:
-        print("An error occured")
+        except IndexError:
+            break
+    print()
+    return num
