c0d14a21d58b43c07795be7ad7fde2a14136308a
fikayo1 <soetandan@gmail.com>
2022-10-13 17:17:13 +0100

    class byte code

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -3,20 +3,20 @@
    containing
    things
 """
-
+from math import pi
 
 class MagicClass():
     """a magic class from byte code"""
+
     def __init__(self, radius):
+        self.__radius = 0
         if type(radius) is not int or type(radius) is not float:
             raise TypeError('radius must be a number')
         else:
             self.__radius == radius
 
     def area(self):
-        from math import pi
         return (self.__radius ** 2) * pi
 
     def circumference(self):
-        from math import pi
         return 2 * pi * self.__radius
