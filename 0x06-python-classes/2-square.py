12c748c475dc3d4b4e583a7467244176b7971904
fikayo1 <soetandan@gmail.com>
2022-10-13 12:26:15 +0100

    vim error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,8 +4,13 @@
 
 class Square():
     """A class that has size as a private attribute"""
+
+
     def __init__(self, size=0):
+        """Constructor of a square with size"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
