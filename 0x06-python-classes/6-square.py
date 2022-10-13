79549521c02f6a65a868b17575e947123f15164a
fikayo1 <soetandan@gmail.com>
2022-10-13 13:56:10 +0100

    error handling

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,7 +33,7 @@ class Square():
     @position.setter
     def position(self, value):
         """setter for private attribute position"""
-        if type(value) is not tuple or len(value) != 2: \
+        if type(value) is not tuple or len(value) != 2 \
                 or type(value[0]) is not int \
                 or type(value[1]) is not int \
                 or value[0] < 0 or value[1] < 0:
