f9f1f84294771e6ca2dfda0b09087d8fcca02913
fikayo1 <soetandan@gmail.com>
2022-10-26 22:34:52 +0100

    pycode style

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """2-is same class module"""
 
+
 def is_same_class(obj, a_class):
     """return true id an object is exactly an instance of a class"""
     return obj.__class__ is a_class
