efd9a8eaf8a434dca41db4b2de82f0fe7f10d000
fikayo1 <soetandan@gmail.com>
2022-10-28 00:16:38 +0100

    students

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Student to JSON."""
+
+
+class Student:
+    """A student class."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return the JSON representation of the student."""
+        if type(attrs) is list and all([type(i) is str for i in attrs]):
+            return {att: self.__dict__[att]
+                    for att in self.__dict__
+                    if att in attrs}
+        return dict(self.__dict__)
+
+    def reload_from_json(self, json):
+        """Set the attributes of a student according to a dictionary."""
+        for att in json:
+            setattr(self, att, json[att])
