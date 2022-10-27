a800fbd8f1cc314f2440bd9551fd4805d60b3ffe
fikayo1 <soetandan@gmail.com>
2022-10-27 23:29:24 +0100

    content

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,6 @@
 def read_file(filename=""):
     """Reads a text file and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        content = f.read()
+        if content != "":
+            print(content[:-1])
