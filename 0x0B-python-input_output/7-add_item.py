b6de317cab77cdba3418d7345d7ee03d7b6c0041
fikayo1 <soetandan@gmail.com>
2022-10-28 00:08:32 +0100

    script

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""Load, add, save."""
-import sys
+"""
+script to save and load
+"""
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-save = __import__("5-save_to_json_file").save_to_json_file
-load = __import__("6-load_from_json_file").load_from_json_file
+filename = 'add_item.json'
+my_list = []
+try:
+    my_list = load_from_json_file(filename)
+except Exception:
+    save_to_json_file(my_list, filename)
 
+arg_len = len(argv)
 
-def main():
-    """Return the exit status of the program."""
-    if len(sys.argv) <= 1:
-        return 0
-    try:
-        old = load("add_item.json")
-    except FileNotFoundError:
-        old = []
-    old += sys.argv[1:]
-    save(old, "add_item.json")
+if arg_len > 1:
+    for i in range(1, arg_len):
+        my_list.append(argv[i])
 
-
-if __name__ == '__main__':
-    sys.exit(main())
+    save_to_json_file(my_list, filename)
