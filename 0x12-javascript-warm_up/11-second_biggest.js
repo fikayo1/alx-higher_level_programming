49df81ac6ee9a622d44b3e9db7203fe1382094fe
fikayo1 <soetandan@gmail.com>
2023-01-07 19:28:42 +0100

    sort

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const nums = process.argv.slide(2).map(
+  function (arg) {
+    return (parseInt(arg));
+  }
+);
+
+nums.sort(function (a, b) { return a - b; });
+
+if (nums.length === 0 || nums.length === 1) {
+  console.log(0);
+} else {
+console.log(nums[nums.length - 2]);
+}
