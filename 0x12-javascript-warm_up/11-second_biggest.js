4d6f122fba13ec827275094f5c3dcce5ffc25638
fikayo1 <soetandan@gmail.com>
2023-01-07 19:33:29 +0100

    sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -11,5 +11,5 @@ nums.sort(function (a, b) { return a - b; });
 if (nums.length === 0 || nums.length === 1) {
   console.log(0);
 } else {
-console.log(nums[nums.length - 2]);
+  console.log(nums[nums.length - 2]);
 }
