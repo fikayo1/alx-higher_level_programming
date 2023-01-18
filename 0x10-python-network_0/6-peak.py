13aff9aed5ea03e6ecf2438e0e8382adc8a31301
fikayo1 <soetandan@gmail.com>
2023-01-19 00:00:41 +0100

    peak files

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,12 @@
+def find_peak(list_of_integers):
+    """
+        find the peak in an unsorted array
+        Args:
+            list_of_integers
+        Return:
+            peak
+    """
+    if len(list_of_integers) == 0:
+        return("None")
+    else:
+        return(max(list_of_integers))
