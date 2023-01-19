db37b8470aef155e5d69a4dac576068536d5f74c
fikayo1 <soetandan@gmail.com>
2023-01-19 14:22:50 +0100

    corrected task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Send a request and display the reponse"""
+"""Get the request ID from the header of a response"""
 import requests
 import sys
 
 if __name__ == "__main__":
-    r = request.get(sys.argv[1])
+    r = requests.get(sys.argv[1])
     print(r.headers.get('X-Request-Id'))
