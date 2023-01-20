bb04de136a33a5474043496f9a238ea202c4add1
fikayo1 <soetandan@gmail.com>
2023-01-20 01:52:17 +0100

    added task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script to get 10 commit from a repo"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    header = {
+        "Accept": "application/vnd.github+json"
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    owner = sys.argv[2]
+    repo = sys.argv[1]
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+    param = {"per_page": "10"}
+    req = requests.get(url, headers=header, params=param)
+    resp = req.json()
+    for commit in resp:
+        print(f"{commit['sha']}: {commit['commit']['author']['name]}")
