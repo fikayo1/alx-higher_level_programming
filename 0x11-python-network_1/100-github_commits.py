1815e72fd0a7f71c4255de11f9d496c9ffb3b7ea
fikayo1 <soetandan@gmail.com>
2023-01-20 02:10:28 +0100

    task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""Lists the 10 most recent commits of a github repo"""
+"""List the 10 most recent commits of a github repository."""
 import requests
 import sys
 
-if __name__ == "__main__":
-    header = {
-        "Accept": "application/vnd.github+json"
-        "X-GitHub-Api-Version": "2022-11-28"
+if __name__ == '__main__':
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28",
     }
-    owner = sys.argv[2]
-    repo = sys.argv[1]
-    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
-    param = {"per_page": "10"}
-    req = requests.get(url, headers=header, params=param)
-    resp = req.json()
-    for commit in resp:
-        print(f"{commit['sha']}: {commit['commit']['author']['name]}")
+    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
+    resp = requests.get(url, params={"per_page": "10"}, headers=headers)
+    commits = resp.json()
+    for commit in commits:
+        print(f"{commit['sha']}: {commit['commit']['author']['name']}")
