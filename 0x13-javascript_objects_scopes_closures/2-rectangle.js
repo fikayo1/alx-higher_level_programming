b5e5aed82fb9d0ef7240361d2a90843d2c582c40
fikayo1 <soetandan@gmail.com>
2023-03-11 16:00:59 +0100

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   contructor (w, h) {
-    if (w > 0 || h > 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
