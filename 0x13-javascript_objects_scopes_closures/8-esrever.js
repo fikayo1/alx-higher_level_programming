211e2ddb0891af49da0c7b978136ae20d85bc544
fikayo1 <soetandan@gmail.com>
2023-03-11 18:42:36 +0100

    task 7

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let new_list = [];
+  const newList = [];
   for (let i = list.length - 1; i >= 0; i--) {
-    new_list.push(list[i]);
-  };
-  return new_list;
+    newList.push(list[i]);
+  }
+  return newList;
 };
